37b36074f44d32a766269ad4879482667c0602d6
Elaine Huang <71571680+huang12211@users.noreply.github.com>
2024-10-28 00:06:37 -0400

    fix: smaller rosa one brain video to comply with github memory limitations

--- src/app/[locale]/(unauth)/design/rosa-brain/page.tsx
+++ src/app/[locale]/(unauth)/design/rosa-brain/page.tsx
@@ -32,7 +32,7 @@ export default function Design() {
                   target="_blank>"
                   className="content-center text-center font-[Georgia] text-4xl text-pink-500"
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-8">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-8">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
                   </svg>
                 </Link>
