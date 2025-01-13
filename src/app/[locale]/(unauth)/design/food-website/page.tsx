a84aebebd876cba1507514cd6e2621a83409396a
Elaine Huang <71571680+huang12211@users.noreply.github.com>
2025-01-12 23:09:18 -0500

    feat: release

--- src/app/[locale]/(unauth)/design/food-website/page.tsx
+++ src/app/[locale]/(unauth)/design/food-website/page.tsx
@@ -19,7 +19,7 @@ export async function generateMetadata() {
 export default function Design() {
   return (
     <>
-      <div className="pb-4">
+      <div className="bg-gray-100 bg-opacity-30">
         <div className="bg-gray-200">
           <div className="mx-auto flex max-w-screen-xl flex-col gap-6 p-16">
             <div className="flex flex-col gap-2">
@@ -30,12 +30,12 @@ export default function Design() {
             </div>
             <div className="caseIntro">
               <p>
-                This is a personal project where I journal all recipes that I have collected from my time with friends and loved ones.
+                This is a personal project where I journal all recipes that I have collected from my time with friends and loved ones. Disclaimer: This is still a work in progress...
               </p>
             </div>
           </div>
         </div>
-        <div className="mx-auto grid max-w-screen-xl gap-10 p-16 md:grid-cols-2 md:grid-rows-1">
+        <div className="mx-auto grid max-w-screen-xl gap-10 p-16 lg:grid-cols-2">
           <div className="flex flex-col gap-2 rounded-lg border border-gray-200 bg-white p-4">
             <h4>The Vision</h4>
             <p className="text-lg">
@@ -45,95 +45,96 @@ export default function Design() {
           </div>
           <div className="flex flex-col gap-2 rounded-lg border border-gray-200 bg-white p-4">
             <h4>My Role</h4>
-            <p className="text-base">
+            <p className="text-lg">
               Designer, developer and principal user.
             </p>
           </div>
         </div>
-        <div className="">
-          <div className="mx-auto flex max-w-screen-xl flex-col gap-4 px-16 py-4 text-lg">
-            <h3 className="text-center">Context</h3>
-            <p>
-              This website's main purpose is to act as a source of inspiration whenever I find myself getting tired of reusing the same 7 recipes that come top of mind when I'm out grocery shopping.
-              It's secondary purpose is the act as a reference for friends and family who ask me for the recipe to a specific dish that I've made for them in the past.
-            </p>
-          </div>
+        <div className="mx-auto flex max-w-screen-xl flex-col gap-4 px-16 py-4 text-lg">
+          <h3 className="pt-0 text-center">Context</h3>
+          <p>
+            This website's main purpose is to act as a source of inspiration whenever I find myself getting tired of reusing the same 7 recipes that come top of mind when I'm out grocery shopping.
+            It's secondary purpose is the act as a reference for friends and family who ask me for the recipe to a specific dish that I've made for them in the past.
+          </p>
         </div>
-        <div className="">
-          <div className="mx-auto flex max-w-screen-xl flex-col gap-4 px-16 py-4 text-lg">
-            <h3 className="text-center">Design Phase</h3>
-            <h4 className="text-lg font-semibold">Wireframes in Figma</h4>
-            <div>
-              <iframe
-                // style={{border: '1px solid rgba(0, 0, 0, 0.1)'}}
-                title="figma low fidelity prototype"
-                width="350"
-                height="650"
-                src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FpijcVhQPrJIuNDDJV64tVJ%2FWireFrames---Elaine's-Eats%3Ftype%3Ddesign%26node-id%3D10-1131%26scaling%3Dscale-down%26page-id%3D10%253A356%26starting-point-node-id%3D10%253A1131"
-                allowFullScreen
-                className="mx-auto max-h-screen"
-              >
-              </iframe>
-            </div>
-          </div>
-        </div>
-        <div className="">
-          <div className="mx-auto flex max-w-screen-xl flex-col gap-4 px-16 py-4 text-lg">
-            <h3 className="text-center">Development Phase</h3>
-            <h3 className="text-lg font-semibold">Tech Stack</h3>
-            <p>
-              The frontend of the website has been written in vanilla Javascript as was built using:
-            </p>
-            <ul className="list-outside list-disc pl-10">
-              <li>Tailwind CSS</li>
-              <li>Heroicons</li>
-            </ul>
-            <p>
-              The backend of the website has been written in Typescript and was built using:
-            </p>
-            <ul className="list-outside list-disc pl-10">
-              <li>Express API</li>
-              <li>Drizzle ORM for better-sqlite3</li>
-            </ul>
-          </div>
-        </div>
-        <div className="">
-          <div className="mx-auto flex max-w-screen-xl flex-col gap-4 px-16 py-4 text-lg">
-            <h3 className="text-center">What I've Learnt</h3>
-            <p>
-              To fill in.
-            </p>
-          </div>
-        </div>
-        <div className="">
-          <div className="grid-row-4 mx-auto my-10 grid max-w-screen-xl gap-4 px-16 text-lg">
-            <h3 className="text-center">Final Product</h3>
-            <p>
-              Please click on the following link to peruse the final product:
-              <Link
-                href="https://huang12211.github.io/ElaineFoodWebsite/"
-                target="_blank>"
-                className="text-sky-500 hover:underline"
-              >
-                {' '}
-                Elaine's Easecipes
-              </Link>
-            </p>
-            <Image
-              src="/assets/images/homePageFoodWebsite.png"
-              alt="recipe page for blueberry muffins"
-              width="600"
-              height="1024"
-              className="mx-auto h-auto w-4/5 md:w-3/5 lg:w-4/6"
-            />
-            <Image
-              src="/assets/images/recipePage.png"
-              alt="recipe page for blueberry muffins"
-              width="400"
-              height="1024"
-              className="mx-auto h-auto w-1/2 md:w-2/5 lg:w-2/6"
+      </div>
+
+      <div className="bg-white">
+        <div className="mx-auto flex max-w-screen-xl flex-col gap-4 px-16 py-4 text-lg">
+          <h3 className="text-center">Design Phase</h3>
+          <h4 className="text-lg font-semibold">Wireframes in Figma</h4>
+          <div>
+            <iframe
+              // style={{border: '1px solid rgba(0, 0, 0, 0.1)'}}
+              title="figma low fidelity prototype"
+              width="350"
+              height="650"
+              src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FpijcVhQPrJIuNDDJV64tVJ%2FWireFrames---Elaine's-Eats%3Ftype%3Ddesign%26node-id%3D10-1131%26scaling%3Dscale-down%26page-id%3D10%253A356%26starting-point-node-id%3D10%253A1131"
+              allowFullScreen
+              className="mx-auto max-h-screen"
             />
           </div>
+        </div>
+      </div>
+      <div className="bg-gray-100 bg-opacity-30">
+        <div className="mx-auto flex max-w-screen-xl flex-col gap-4 px-16 py-4 text-lg">
+          <h3 className="text-center">Development Phase</h3>
+          <h3 className="text-lg font-semibold">Tech Stack</h3>
+          <p>
+            The frontend of the website has been written in vanilla Javascript as was built using:
+          </p>
+          <ul className="list-outside list-disc pl-10">
+            <li>Tailwind CSS</li>
+            <li>Heroicons</li>
+          </ul>
+          <p>
+            The backend of the website has been written in Typescript and was built using:
+          </p>
+          <ul className="list-outside list-disc pl-10">
+            <li>Express API</li>
+            <li>Drizzle ORM for better-sqlite3</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="bg-white">
+        <div className="mx-auto flex max-w-screen-xl flex-col gap-4 px-16 py-4 text-lg">
+          <h3 className="text-center">What I've Learnt</h3>
+          <p>
+            This product gave the me chance to learn the essentials web development and has facilitated my communication with the software developpers that I work with on a day to day basis.
+            It's been the project through which I've been self-teaching front-end, back-end and general web development concepts.
+          </p>
+        </div>
+      </div>
+
+      <div className="bg-gray-100 bg-opacity-30">
+        <div className="grid-row-4 mx-auto my-10 grid max-w-screen-xl gap-4 px-16 text-lg">
+          <h3 className="text-center">The Current Product</h3>
+          <p>
+            Please click on the following link to peruse the current state of the website:
+            <Link
+              href="https://huang12211.github.io/ElaineFoodWebsite/"
+              target="_blank>"
+              className="text-sky-500 hover:underline"
+            >
+              {' '}
+              Elaine's Easecipes
+            </Link>
+          </p>
+          <Image
+            src="/assets/images/homePageFoodWebsite.png"
+            alt="recipe page for blueberry muffins"
+            width="600"
+            height="1024"
+            className="mx-auto h-auto w-full rounded-xl border border-gray-200"
+          />
+          <Image
+            src="/assets/images/recipePage.png"
+            alt="recipe page for blueberry muffins"
+            width="400"
+            height="1024"
+            className="mx-auto h-auto w-1/2 rounded-xl border border-gray-200 md:w-2/5 lg:w-2/6"
+          />
         </div>
       </div>
     </>
