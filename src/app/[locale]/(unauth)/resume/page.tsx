3c5006b3c5a33c30a1cf3e59afed4651eb390d98
Elaine Huang <71571680+huang12211@users.noreply.github.com>
2025-12-24 18:53:14 -0500

    fix: color of resume button

--- src/app/[locale]/(unauth)/resume/page.tsx
+++ src/app/[locale]/(unauth)/resume/page.tsx
@@ -200,7 +200,7 @@ export default function Resume(props: { params: { locale: string } }) {
         <Link
           href="/assets/files/Elaine_Huang_CV.pdf"
         >
-          <div className="mx-auto my-6 flex w-3/5 items-center justify-center gap-2 rounded-full border-4 border-gray-700 px-6 py-2 text-base shadow-xl duration-150 hover:scale-110 hover:bg-purple-400 hover:font-bold md:w-[45%] md:text-xl">
+          <div className="mx-auto my-6 flex w-3/5 items-center justify-center gap-2 rounded-full border-4 border-gray-700 px-6 py-2 text-base shadow-xl duration-150 hover:scale-110 hover:bg-teal-600 hover:font-bold md:w-[45%] md:text-xl">
             <p>Download CV </p>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="mb-2 inline size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
